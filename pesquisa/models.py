99af35b846229d70416c134ffc80996b6a2b6976
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-08-31 13:59:13 -0300

    Alterações

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Produto(models.Model):
-   nome = models.CharField(max_length=50)
-   fab =  models.CharField(max_length=50)
-   quantidade =  models.IntegerField()
+   nome = models.CharField(max_length=255)
+   fab =  models.CharField(max_length=255)
+   quantidade = models.IntegerField()
 
    def __str__(self):
       return self.nome
