ed0cd5016393a2599f921f4f754310941299a05f
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-08-31 09:58:50 -0300

    Bora

--- Pesquisa/produtos/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from produtos import models
-
-class ProdutosSerializers(serializers.ModelSerializer):
-    class Meta:
-       model = models.Produto
-       fields = '__all__'
