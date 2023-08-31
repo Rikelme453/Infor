a961b073d2d8f7e2bf8ffe077fb0877680d780bc
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-08-31 15:53:01 -0300

    Alterações?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,34 +2,42 @@ import React from 'react';
 import Search from './components/Search.jsx';
 import './App.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  
+
   return (
 
     <>
-      
+
       <div>
         <header>
           <div className="header">
             <a id="navText" className="navbar-brand" href="base.html">INFOR HOME</a>
             <ul class="suggestions">
               <Search />
+              <div className="lupa_div">
+
+                <FontAwesomeIcon icon={faSearch} className='lupa' />
+
+              </div>
+
             </ul>
             <p class="navPg">Seja Bem-vindo!</p>
             <div class="link">
               <a href="#">Entrar |</a>
               <a href="%">Cadastrar</a>
             </div>
-            </div>
-            <div className="SubNav">
-              <a href="#">Categorias</a>
-              <a href="#">Carrinho</a>
-              <a href="#">loja</a>
-              <a href="#">Contato</a>
-              <a href="#">Quem Somos?</a>
-            </div>
-            
+          </div>
+          <div className="SubNav">
+            <a href="#">Categorias</a>
+            <a href="#">Carrinho</a>
+            <a href="#">loja</a>
+            <a href="#">Contato</a>
+            <a href="#">Quem Somos?</a>
+          </div>
+
         </header>
       </div>
     </>
