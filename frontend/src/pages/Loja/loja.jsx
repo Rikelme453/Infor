028775104a3dde7f7cb2468c071dc74242c7fc5b
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-09-01 14:35:34 -0300

    Adicionada div Produto

--- frontend/src/pages/Loja/loja.jsx
+++ frontend/src/pages/Loja/loja.jsx
@@ -1,7 +1,34 @@
-
 import React from 'react'
+import { useEffect, useState } from "react";
+
+
+
+const url =`http://127.0.0.1:8000/`
 
 function Loja(){
+    const [data, setData] = useState([])
+    useEffect(() => {
+       fetchData()
+       
+    }, [])
+
+   const fetchData = async() => {
+     const endpoint = `${url}`
+   
+    try {
+        const response = await fetch (endpoint , {
+          method: 'GET'
+        })
+
+        const data = await response.json()
+        console.log(data)
+        setData(data)
+    }
+     catch (e){
+       console.log(e)
+     }
+           
+    } 
    return(
     <>
         
@@ -9,6 +36,14 @@ function Loja(){
               
          <body>
               <h1>Página da Loja</h1>
+ 
+              <div className='ProdutoDiv'>
+                    <ul>
+                        
+                     {data && data.map(el => <li key = {el.id}> {el.nome} </li> )}
+             
+                    </ul>
+              </div>
          </body>      
               
      </div>
