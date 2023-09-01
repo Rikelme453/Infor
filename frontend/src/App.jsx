9021edbafca5bbb14e85f586e31d0ce85840d829
marcondes <marcondesdeev@gmail.com>
2023-09-01 11:22:00 -0300

    MAM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,60 @@
 import React from 'react';
 import Search from './components/Search.jsx';
 import './App.css'
-
+import Routes from './Routes.jsx'
+import Loja from './pages/Loja/loja'
+import Home from './pages/Home/home'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCartShopping, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
-  
 
   return (
 
-    
-
     <>
-
+     
       <div>
         <header>
           <div className="header">
             <a id="navText" className="navbar-brand" href="base.html">INFOR HOME</a>
-            <ul className="suggestions">
+            <ul class="suggestions">
               <Search />
+              <div className="lupa_div">
+
                 <FontAwesomeIcon icon={faSearch} className='lupa' />
+
+              </div>
+
+
+
             </ul>
-            <FontAwesomeIcon icon={faUser} className='user'/> 
-            <p className="navPg">Seja Bem-vindo!</p>
-            <div className="link">
-              <a href="#">Entrar |  </a>
+            <p class="navPg">Seja Bem-vindo!</p>
+            <div class="link">
+              <a href="#">Entrar |</a>
               <a href="%">Cadastrar</a>
             </div>
-            <FontAwesomeIcon icon={faCartShopping} className='cart'/>
           </div>
           <div className="SubNav">
-            <a href="#">Categorias<FontAwesomeIcon icon={faBars} className='cat'/></a>
+            <a href="#">Categorias</a>
             <a href="#">Carrinho</a>
-            <a href="#">loja</a>
+            <a href="/loja" element = {<Loja />} >loja</a>
             <a href="#">Contato</a>
             <a href="#">Quem Somos?</a>
           </div>
 
         </header>
       </div>
+    <br />
+    <br/>
+    <br/>
+
+    <Routes />
     </>
+   
+      
+      
 
   );
 }
