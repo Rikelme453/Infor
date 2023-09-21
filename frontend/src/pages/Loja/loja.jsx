659c856106509a35b0b3bf3b66115227d9bf1ee7
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-09-21 11:21:31 -0300

    Atualizações Substanciais

--- frontend/src/pages/Loja/loja.jsx
+++ frontend/src/pages/Loja/loja.jsx
@@ -1,52 +1,57 @@
 import React from 'react'
 import { useEffect, useState } from "react";
+import './loja.css';
 
 
 
 const url = 'http://127.0.0.1:8000/api/produtos/';
 
 
-function Loja(){
-    const [data, setData] = useState([])
-    useEffect(() => {
-       fetchData()
-       
-    }, [])
+function Loja() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetchData()
 
-   const fetchData = async() => {
-     const endpoint = `${url}`
-   
+  }, [])
+
+  const fetchData = async () => {
+    const endpoint = `${url}`
+
     try {
-        const response = await fetch (endpoint , {
-          method: 'GET'
-        })
+      const response = await fetch(endpoint, {
+        method: 'GET'
+      })
 
-        const data = await response.json()
-        console.log(data)
-        setData(data)
+      const data = await response.json()
+      console.log(data)
+      setData(data)
     }
-     catch (e){
-       console.log(e)
-     }
-           
-    } 
-   return(
+    catch (e) {
+      console.log(e)
+    }
+
+  }
+  return (
     <>
-        
-     <div>
-              
-         <body>
-              <h1>Página da Loja</h1>
-               
-                   <section className='produto-section'>
-                      {data && data.map(el => <div className='ProdutoDiv' key = {el.id_produto}>
-                      <img className='imagem' src= {el.image}  ></img><p>{el.nome}</p></div> )}
-                   </section> 
-         </body>                   
-     </div>
-    
+
+      <div>
+
+        <body>
+          <h1>Página da Loja</h1>
+
+          <section className='produto-section'>
+            {data && data.map(el => <div className='ProdutoDiv' key={el.id_produto}>
+              <img className='imagem' src={el.image}></img>
+
+              <div className='elementos'><h4 className='Nome'>{el.nome}</h4>
+                <button className='botao-compra'>Comprar</button></div>
+            </div>)}
+          </section>
+        </body>
+      </div>
+
     </>
 
-   )
+  )
 }
 export default Loja;
