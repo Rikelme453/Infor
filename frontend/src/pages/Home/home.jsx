e429c6208dc822565c52bf7ac0817cc0a1289b5c
marcondes <marcondesdeev@gmail.com>
2023-09-21 13:10:14 -0300

    mais correção

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import Search from '../../components/Search';
 import Footer from '../../components/footer';
 import './home.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCartShopping, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
-import App from '../../App';
-import Loja from '../Loja/loja';
 import indeximage from '../../assets/indexImage.svg'
 import './home.css';
-import Cadastro from '../Cadastro/Cadastro';
-import Login from '../login/Login';
 import { Menu } from '../../components/Menu';
 
 
