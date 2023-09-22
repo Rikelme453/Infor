fe4c09183b5af84b700bb4b62f66bf3d3b3f6e5e
Ruivo.dev <skysystemenio@gmail.com>
2023-09-22 08:47:26 -0300

    pagina home

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -3,6 +3,7 @@ import Search from '../../components/Search';
 import Footer from '../../components/footer';
 import './home.css'
 import indeximage from '../../assets/indexImage.svg'
+import indeximage2 from '../../assets/indexImage2.svg'
 import './home.css';
 import { Menu } from '../../components/Menu';
 
@@ -22,9 +23,17 @@ function Home() {
                <img className='indexImage' src={indeximage} draggable='false'></img>
             </main>
          </div>
-         <br />
-         <br />
-         <br />
+         <div className='container'>
+    <div className='textHome'>
+    Bem-vindo à InforHome, o seu destino<br></br>definitivo para todas as suas necessidades<br></br>tecnológicas e de casa!<br></br>Estamos empolgados por você estar aqui<br></br>e ansiosos para mostrar tudo o que nossa<br></br>loja tem a oferecer.
+
+<br></br><br></br>Na InforHome, acreditamos que a tecnologia<br></br>deve tornar sua vida mais fácil e mais divertida.<br></br>É por isso que oferecemos uma ampla gama<br></br>de produtos eletrônicos, dispositivos<br></br>inteligentes para casa, eletrodomésticos<br></br>e muito mais.
+
+    </div>
+    <img src={indeximage2} className='indexImage2'></img>
+         </div>
+
+
          <Footer />
       </>
    );
